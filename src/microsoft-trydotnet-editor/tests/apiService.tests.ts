706c8ebdb4242da36b2a21fb9d6b6d99b966a508
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2026-05-16 15:10:29 -0700

    chore: remove .NET Foundation copyright header from new file, add AGENTS.md
    New files added to this fork should not carry the .NET Foundation header.
    Existing files are left unchanged.

--- src/microsoft-trydotnet-editor/tests/apiService.tests.ts
+++ src/microsoft-trydotnet-editor/tests/apiService.tests.ts
@@ -1,6 +1,3 @@
-// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 import { expect } from "chai";
 import { describe } from "mocha";
 import { createApiService } from "../src/apiService";
